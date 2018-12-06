aff382805062f2a0500d540debb85acfe7a34715
TySanteOn <dante.verbauwhede@gmail.com>
2018-12-06 14:21:41 +0100

    player on screen & licht optimalisation

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+import Player from "./classes/Player.js"
 {
   let loader,
     scene,
@@ -10,7 +11,12 @@
     aspectRatio,
     renderer,
     container,
-    pointLight;
+    pointLight,
+    player1,
+    playerX = 100,
+    playerY = 200,
+    playerZ = 20,
+    overview = false;
 
 
   const init = () => {
@@ -19,20 +25,44 @@
     loadMaze();
 
     createPulseLight();
+    createPlayer();
 
     getMicVolume();
 
     loop();
 
-    document.addEventListener('keydown', handleKeyDown);
+    document.getElementById(`cameraFullMaze`).addEventListener('click', handleButtonClick);
+    document.addEventListener('keypress', handleKeyDown);
+  };
+
+  const handleButtonClick = () => {
+    overview = !overview;
+    if (overview) {
+      camera.position.set(500, 2500, 500);
+      camera.rotation.x = 300;
+      camera.rotation.y = 0;
+    }
+    if (!overview) {
+      camera.position.set(playerX, playerY, playerZ);
+      camera.rotation.x = 0;
+    }
+  };
+
+  const createPlayer = () => {
+    const x = 50;
+    const y = 200;
+    const z = 20;
+
+    player1 = new Player(x, y, z);
+    scene.add(player1.mesh);
   };
 
   const createPulseLight = () => {
-    const xPos = 0;
+    const xPos = 100;
     const yPos = 200;
-    const zPos = 0;
+    const zPos = 20;
 
-    pointLight = new THREE.PointLight(0xffffff, 1, 0, 2);
+    pointLight = new THREE.PointLight(0xffffff, 1, 1000, 4);
     pointLight.position.set(xPos, yPos, zPos);
     pointLight.castShadow = true;
     scene.add(pointLight);
@@ -53,8 +83,9 @@
     );
 
     camera.position.x = 100;
-    camera.position.y = 160;
+    camera.position.y = 200;
     camera.position.z = 20;
+
   }
     
   const createScene = () => {
@@ -76,9 +107,6 @@
 
     container = document.getElementById('world');
     container.appendChild(renderer.domElement);
-
-    renderer.domElement.addEventListener('mousedown', (e, renderer, camera) => downClbk(e, renderer, camera));
-    renderer.domElement.addEventListener('mouseup', (e, renderer, camera) => upClbk(e, renderer, camera));
   };
 
   const loop = () => {
@@ -90,6 +118,20 @@
   const loadMaze = () => {
     loader = new THREE.ObjectLoader();
     loader.load('data/maze-floor.dae.json', object => {
+      console.log(object);
+      object.children.forEach(child => {
+        child.castShadow = true;
+        child.receiveShadow = true;
+      });
+      // object.traverse(child => {
+      //       if (child instanceof THREE.Mesh) {
+      //         child.castShadow = true;
+      //         child.receiveShadow = true;
+      //       }
+      //     });
+      // daemesh = object.children[0].children[0];
+      // daemesh.castShadow = true;
+      // daemesh.receiveShadow = true;
       scene.add(object);
     });
   };
@@ -98,31 +140,43 @@
     let vector;
     vector = camera.getWorldDirection(vector);
     const angle = Math.atan2(vector.z, vector.x) * -1;
-    console.log(angle);
+    
+    switch (e.keyCode) {
+      case 37:
+        camera.rotation.y += (10 * Math.PI) / 180;
+        break;
+      case 39:
+        camera.rotation.y -= (10 * Math.PI) / 180;
+        break;
+      case 38:
+        playerX += Math.cos(angle) * 20;
+        playerZ -= Math.sin(angle) * 20;
+        break;
+      case 40:
+        playerX -= Math.cos(angle) * 20;
+        playerZ += Math.sin(angle) * 20;
+        break;
+    }
 
-    if (e.keyCode === 37) {
-      camera.rotation.y += 10 * Math.PI / 180;
+    if (overview) {
+      camera.position.set(500, 2500, 500);
+      camera.rotation.x = 300;
     }
-    if (e.keyCode === 39) {
-      camera.rotation.y -= 10 * Math.PI / 180;
-    }
-    if (e.keyCode === 38) {
-      camera.position.x += Math.cos(angle) * 20;
-      camera.position.z -= Math.sin(angle) * 20;
-    }
-    if (e.keyCode === 40) {
-      camera.position.x -= Math.cos(angle) * 20;
-      camera.position.z += Math.sin(angle) * 20;
+    if (!overview) {
+      camera.position.set(playerX, playerY, playerZ);
+      camera.rotation.x = 0;
     }
 
-    pointLight.position.set(camera.position.x, camera.position.y, camera.position.z);
+    pointLight.position.set(playerX, playerY, playerZ);
+    player1.mesh.position.set(playerX - 50, playerY, playerZ);
+    
 
     renderer.render(scene, camera);
   }
 
   const editLightPower = () => {
     if (micIsOn){
-      pointLight.power = volume * 20;
+      pointLight.power = volume * 40;
     }
   }
 
